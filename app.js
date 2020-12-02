fd9113430105bb8bfd8798bb8ae035ebf34792d6
Kishan Mandora <kishanmandora7@gmail.com>
2020-12-02 09:57:38 +0530

    fix: change the url

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const inputTxt = document.querySelector("#input-text");
 const translateBtn = document.querySelector("#translate");
-const outputTxt = document.querySelector("output-text");
+const outputTxt = document.querySelector("#output-text");
 
-const url = "https://jsonplaceholder.typicode.com/todos";
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+translateBtn.addEventListener("click", fetchData);
+
+function err(error) {
+  alert("something wrong due to error " + error);
+}
+
+function fetchData(text) {
+  const newUrl = `${url}?text=${inputTxt.value}`;
+  fetch(newUrl)
+    .then((rep) => rep.json())
+    .then((json) => {
+      outputTxt.innerText = json.contents.translated;
+      console.log(json);
+    })
+    .catch(err);
+
+  inputTxt.value = "";
+}
