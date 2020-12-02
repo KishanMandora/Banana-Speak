57d193aade5378833417d068c802a37bf253df05
Kishan Mandora <kishanmandora7@gmail.com>
2020-12-02 09:30:51 +0530

    add queryselectors and fake url

---
+++ app.js
@@ -0,0 +1,5 @@
+const inputTxt = document.querySelector("#input-text");
+const translateBtn = document.querySelector("#translate");
+const outputTxt = document.querySelector("output-text");
+
+const url = "https://jsonplaceholder.typicode.com/todos";
